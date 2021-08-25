7e7e61aa28a5ffca0da9cc5b674d0f7306e9d74e
isis <icey1727@gmail.com>
2021-08-25 19:50:45 -0400

    Adds routes for products and all products action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,7 @@
 class ProductsController < ApplicationController
+def all
+  product = Product.all
+  render json: product
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ get "/all_products", controller: "products", action: "all"
+
+ get "/headphones", controller: "products", action: "headphones"
+
+ get "/hats", controller: "products", action: "hats"
+
+ get "/books", controller: "products", action: "books"
 end
