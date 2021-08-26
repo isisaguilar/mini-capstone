b5aed29d8e726b0ef24644469a0e63eb1395ff52
isis <icey1727@gmail.com>
2021-08-25 20:00:06 -0400

    adds rest of products actions in controller file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,21 @@
 class ProductsController < ApplicationController
-def all
-  product = Product.all
-  render json: product
-end
+  def all
+    product = Product.all
+    render json: product
+  end
 
+  def headphones
+    product = Product.first
+    render json: product
+  end
+
+  def hats
+    product = Product.find(2)
+    render json: product
+  end
+
+  def books
+    product = Product.find(3)
+    render json: product
+  end
 end
